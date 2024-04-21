9ff6dfd9b2074725dacb4a667fb0caa30144336f
RajAdarsh2022 <inforaj.2022@gmail.com>
2024-04-21 14:56:13 +0530

    Implemented admin routes with jwt authentication

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,23 @@
+const jwt = require('jsonwebtoken')
+const jwtSecretKey = '<yourSecretKey>'
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.authorization
+    const requiredToken = token.slice(7)
+    console.log(requiredToken)
+    
+    try {
+        jwt.verify( requiredToken , jwtSecretKey)
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(403).json({ mssg : "Bad authentication request"})
+    }
+
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,87 @@
-const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
-const router = Router();
+const { Router } = require("express")
+const {Admin , Course} = require("../db/index")
+const adminMiddleware = require("../middleware/admin")
+const router = Router()
+
+const jwt = require('jsonwebtoken')
+const jwtSecretKey = '<yourSecretKey>'
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    Admin.create({
+        username,
+        password
+    })
+    .then(function(newAdmin){
+        res.json({
+            mssg : `New admin created successfully`,
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({mssg : "Error in creating admin"})
+    })
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    const user = await Admin.findOne({
+        username,
+        password
+    })
+
+    if(user){
+        const token = jwt.sign({username : username}, jwtSecretKey)
+        console.log(token)
+        res.json({token : token})
+    }
+    else{
+        res.status(403).json({ mssg : "Bad authentication"})
+    }
+
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title = req.body.title
+    const description = req.body.description
+    const price = req.body.price
+    const imageLink = req.body.imageLink
+
+    Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+    .then((newCourse) => {
+        res.json({ mssg : "Course added successfully!"})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({ mssg : "unable to add course"})
+    })
+
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find()
+    .then((allCourses) => {
+        res.json({ courses : allCourses})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({ mssg : "Unable to fetch courses!"})
+    })
 });
 
 module.exports = router;

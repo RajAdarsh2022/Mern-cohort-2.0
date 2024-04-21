0a35b9dce4be37aa00c2a7587c17523e7f84a7ca
RajAdarsh2022 <inforaj.2022@gmail.com>
2024-04-21 18:53:07 +0530

    Implemented user routes with jwt authentication

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,24 @@
+const jwt = require('jsonwebtoken')
+const jwtSecretKey = '<yourSecretKey>'
+
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.authorization
+    const requiredToken = token.slice(7)
+
+    // console.log(requiredToken)
+    try {
+        jwt.verify(requiredToken , jwtSecretKey)
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(403).json({ mssg : "Invalid user"})
+    }
+    
+
 }
 
 module.exports = userMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,26 +1,117 @@
-const { Router } = require("express");
-const router = Router();
-const userMiddleware = require("../middleware/user");
+const { Router } = require("express")
+const {User , Course} = require("../db/index")
+const router = Router()
+const userMiddleware = require("../middleware/user")
+
+const jwt = require('jsonwebtoken')
+const jwtSecretKey = '<yourSecretKey>'
 
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    User.create({
+        username,
+        password
+    })
+    .then((newUser) => {
+        res.json({ mssg : `New user added !`})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({ mssg : "unable to add user"})
+    })
+
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    const user = await User.findOne({
+        username,
+        password
+    })
+
+    if(user){
+        const userToken = jwt.sign({username : username} , jwtSecretKey);
+        res.json({ token : userToken})
+    }
+    else{
+        res.status(403).json({ mssg : "Invalid user!"})
+    }
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find()
+    .then((allCourses) => {
+        res.json({ courses : allCourses})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({ mssg : "Unable to fetch courses!"})
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+
+    const token = req.headers.authorization
+    const requiredToken = token.slice(7)
+    const decodedPayload = jwt.decode(requiredToken)
+
+    //getting the username
+    const username  = decodedPayload.username
+    const courseId = req.params.courseId
+
+    User.updateOne({username} , {
+        "$push" : {purchasedCourses : courseId}
+    })
+    .then((r) => {
+        res.json({mssg : "Course Added succesfully"})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({ mssg : "unable to add courses"})
+    })
+
+
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const token = req.headers.authorization
+    const requiredToken = token.slice(7)
+    const decodedPayload = jwt.decode(requiredToken)
+
+    //getting the username
+    const username  = decodedPayload.username
+    
+    const user = await User.findOne({username})
+    if(user){
+        Course.find({
+            _id : {"$in" : user.purchasedCourses}
+        })
+        .then((allCoursesPurchasedByUser) => {
+            res.json({courses : allCoursesPurchasedByUser})
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({ mssg : "Unable to find courses"})
+        })
+    
+    }
+    else{
+        console.log(error)
+        res.status(500).json({ mssg : "Unable to get bought courses"});
+    }
+
+
+
 });
 
 module.exports = router
